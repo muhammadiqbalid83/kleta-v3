0fc3721fea78a878af5ca228d67dc0b60e989269
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-10-27 07:46:26 +0700

    update text

--- components/Header/GlobalNotification.jsx
+++ components/Header/GlobalNotification.jsx
@@ -2,10 +2,10 @@ export default function GlobalNotification() {
   return (
     <div className="w-full h-auto bg-[#1a4845] text-white text-[12px]">
       <div className="text-center py-[10px]">
-        SUMMER SALE FOR ALL SWIM SUITS AND FREE EXPRESS INTERNATIONAL DELIVERY -
-        OFF 50%!{' '}
+        PENJUALAN MUSIM PANAS UNTUK SEMUA PAKAIAN RENANG DAN PENGIRIMAN
+        INTERNASIONAL EKSPRES GRATIS - DISKON 50%!{' '}
         <a href="#" className="font-bold">
-          SHOP NOW
+          BELANJA SEKARANG
         </a>
       </div>
     </div>
