b56f72d8469d510f268ada1b899ba497b934a256
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-10-27 07:42:11 +0700

    add topbar component

--- components/Header/Topbar.jsx
+++ components/Header/Topbar.jsx
@@ -7,55 +7,109 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 
+import { BsInstagram } from 'react-icons/bs'
+import {
+  BiLogoFacebook,
+  BiLogoTwitter,
+  BiSolidDownArrow,
+  BiLogoInstagram,
+  BiLogoLinkedin,
+} from 'react-icons/bi'
+
 export default function Topbar() {
   return (
     <div className="w-full h-auto">
       <div className="container flex justify-between text-[13px] py-[10px]">
-        <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger>3.1 Followers</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+        <div className="flex gap-3 items-center">
+          <div>
+            <BsInstagram size={17} />
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="flex items-center gap-2">
+                  <div>3.1 Followers</div>
+                  <div>
+                    <BiSolidDownArrow size={8} />
+                  </div>
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator /> */}
+                <DropdownMenuItem>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <BiLogoFacebook />
+                    </div>
+                    <div>Facebook</div>
+                  </div>{' '}
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <BiLogoTwitter />
+                    </div>
+                    <div>Twitter</div>
+                  </div>{' '}
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <BiLogoInstagram />
+                    </div>
+                    <div>Instagram</div>
+                  </div>{' '}
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <BiLogoLinkedin />
+                    </div>
+                    <div>Linkedin</div>
+                  </div>{' '}
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
         <div>
-          Free Shipping World wide for all orders over $199.{' '}
+          Gratis ongkir ke seluruh dunia dengan minimal pembelian 2 Juta.{' '}
           <a href="#" className="text-[#ee403d]">
-            Click and Shop Now.
+            Belanja Sekarang.
           </a>
         </div>
-        <div className="flex">
-          <div>Order Tracking</div>
+        <div className="flex gap-3">
+          <div>Lacak Pesanan</div>
           <div>
             {' '}
             <DropdownMenu>
-              <DropdownMenuTrigger>English</DropdownMenuTrigger>
+              <DropdownMenuTrigger>
+                <div className="flex items-center gap-2">
+                  <div>Indonesia</div>
+                  <div>
+                    <BiSolidDownArrow size={8} />
+                  </div>
+                </div>
+              </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuItem>English</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
           <div>
             <DropdownMenu>
-              <DropdownMenuTrigger>USD</DropdownMenuTrigger>
+              <DropdownMenuTrigger>
+                {' '}
+                <div className="flex items-center gap-2">
+                  <div>IDR</div>
+                  <div>
+                    <BiSolidDownArrow size={8} />
+                  </div>
+                </div>
+              </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
+                <DropdownMenuItem>USD</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
